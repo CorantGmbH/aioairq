89d0a503622823f365c75a538d2d4281710558a6
Renat Sibgatulin <renat.sibgatulin@gmail.com>
2022-08-11 17:18:33 +0200

    The version which was published to PyPI

--- aioairq/__init__.py
+++ aioairq/__init__.py
@@ -4,6 +4,6 @@ __credits__ = "Corant GmbH"
 __email__ = "daniel.lehmann@corant.de"
 __url__ = "https://www.air-q.com"
 __license__ = "Apache License 2.0"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from aioairq.core import AirQ  # to allow more direct access: `from aioairq import AirQ`

1264a9b1fc9a549d3f8701ad49b142adbcd9ffc0
theHacker <git@thehacker.biz>
2024-02-21 00:06:38 +0100

    Bump version to 0.4.0

--- aioairq/__init__.py
+++ aioairq/__init__.py
@@ -4,7 +4,7 @@ __credits__ = "Corant GmbH"
 __email__ = "daniel.lehmann@corant.de"
 __url__ = "https://www.air-q.com"
 __license__ = "Apache License 2.0"
-__version__ = "0.3.2"
+__version__ = "0.4.0"
 __all__ = [
     "AirQ",
     "DeviceInfo",

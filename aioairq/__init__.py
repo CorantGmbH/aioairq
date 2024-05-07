78c1d3703defd2f868ae60e8ea13e0bd24b9e97a
Renat Sibgatulin <renat.sibgatulin@gmail.com>
2024-05-07 12:33:16 +0200

    Bump the version to 0.4.1

--- aioairq/__init__.py
+++ aioairq/__init__.py
@@ -1,10 +1,11 @@
 """air-Q library."""
+
 __author__ = "Daniel Lehmann"
 __credits__ = "Corant GmbH"
 __email__ = "daniel.lehmann@corant.de"
 __url__ = "https://www.air-q.com"
 __license__ = "Apache License 2.0"
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 __all__ = [
     "AirQ",
     "DeviceInfo",
